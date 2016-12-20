9e2c44d795ac5cd593b3e2a3912767b99d55083f
Corbin <corbin.coleman@holbertonschool.com>
2016-12-20 04:02:49 +0000

    Added _atoi function

--- string_more.c
+++ string_more.c
@@ -33,3 +33,25 @@ char *_strcat(char *frontstr, char *backstr)
         newstr[newstrpos] = '\0';
         return (newstr);
 }
+
+/**
+ * _atoi - Convert string into an integer
+ * @str: String to convert to int
+ *
+ * Return: The int the string represents,
+ * -1 if there are not only numbers in str
+ */
+int _atoi(char *str)
+{
+        int i, num;
+
+        i = num = 0;
+        while (str[i] != '\0')
+        {
+                if (str[i] < '0' || str[i] > '9')
+                        return (-1);
+                num = num * 10;
+                num += str[i] - '0';
+        }
+        return (num);
+}
